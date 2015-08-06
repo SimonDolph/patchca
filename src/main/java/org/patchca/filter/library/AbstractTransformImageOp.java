a39d5a4e672e43dc22f832a44b94bf82e561b69e
SimonDolph <simon.dong.sea@hotmail.com>
2015-08-06 09:03:44 +0800

    code style & unused code

--- src/main/java/org/patchca/filter/library/AbstractTransformImageOp.java
+++ src/main/java/org/patchca/filter/library/AbstractTransformImageOp.java
@@ -22,24 +22,23 @@ package org.patchca.filter.library;
 public abstract class AbstractTransformImageOp extends AbstractImageOp {
 
 	protected abstract void transform(int x, int y, double[] t);
-	
+
 	protected void init() {
 	}
 
 	private boolean initialized;
-	
+
 	public AbstractTransformImageOp() {
 		setEdgeMode(EDGE_CLAMP);
 	}
-	
+
 	@Override
 	protected void filter(int[] inPixels, int[] outPixels, int width, int height) {
 		if (!initialized) {
 			init();
 			initialized = true;
 		}
-		long time1 = System.currentTimeMillis();
-		double[]t = new double[2];
+		double t[] = new double[2];
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
 				transform(x, y, t);
@@ -47,9 +46,5 @@ public abstract class AbstractTransformImageOp extends AbstractImageOp {
 				outPixels[x + y * width] = pixel;
 			}
 		}
-		long time2 = System.currentTimeMillis() - time1;
-		//System.out.println("AbstractTransformImageOp " + time2);
 	}
-	
 }
-
